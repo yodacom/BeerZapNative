c7d8f7ca1623cc6b83ab8c95a1b52fde0d001f62
Jeremy Black <jb@yodacom.com>
2016-07-02 10:30:04 -0600

    initial commit
    initial base commit

---
+++ server/imports/oauth-facebook.js
@@ -0,0 +1,21 @@
+// server/imports/oauth-facebook.js
+
+import { ServiceConfiguration } from 'meteor/service-configuration';
+import { Meteor } from 'meteor/meteor';
+
+const settings = Meteor.settings.oauth.facebook;
+
+const init = () => {
+  if (!settings) return;
+  ServiceConfiguration.configurations.upsert(
+    { service: "facebook" },
+    {
+      $set: {
+        appId: settings.appId,
+        secret: settings.secret
+      }
+    }
+  );
+}
+
+export default init;
